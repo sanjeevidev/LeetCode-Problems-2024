47ed41413a1a608d7fadc9e40ee26c9d58119589
Sanjeevi Kumar V <sanjeevikumar116@gmail.com>
2024-01-01 18:59:48 +0530

    Jan 1

---
+++ January/Assign Cookies.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findContentChildren(self, g, s):
+        g.sort()
+        s.sort()
+        index = 0
+        cnt = 0
+        
+        while index < len(s) and cnt < len(g):
+            if s[index] >= g[cnt]:
+                cnt += 1
+            index += 1
+            
+        return cnt
